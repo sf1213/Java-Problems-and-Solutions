0cec7b67be8ecee4df900cc6b9882554b696e1e9
Alasse <alasse_1988@126.com>
2018-07-05 13:30:16 +0800

    0705

---
+++ LinkedList/Delete_Node_in_a_Linked_List_237.java
@@ -0,0 +1,8 @@
+package LinkedList;
+
+public class Delete_Node_in_a_Linked_List_237 {
+	public void deleteNode(ListNode node) {     
+        node.val=node.next.val;
+        node.next=node.next.next;      
+    }
+}
