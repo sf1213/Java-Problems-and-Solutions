43c7b134c4d9bccd8675724ea8bbcec3be3cab59
Alasse <alasse_1988@126.com>
2018-07-07 13:59:54 +0800

    0707

--- Array/Pascals_Triangle_118.java
+++ Array/Pascals_Triangle_118.java
@@ -30,4 +30,30 @@ public class Pascals_Triangle_118 {
 		return allrows;
 
 	}
+	
+	
+	//Pascal's Triangle II-ArrayList
+	public List<Integer> getRow(int rowIndex) {
+        List<Integer> row=new ArrayList<Integer>();
+        for(int i=0;i<=rowIndex;i++){
+            //row.add(0,1);  // cost O(N)
+            for(int j=i-1;j>0;j--){    //from right to left
+                row.set(j,row.get(j)+row.get(j-1));
+            }
+            row.add(1);   // cost O(1)
+        }
+        return row;
+    }
+	//Pascal's Triangle II-Arrays
+	public List<Integer> getRow(int rowIndex) {       
+        Integer[] row=new Integer[rowIndex+1];
+        Arrays.fill(row,1);
+        for(int i=1;i<rowIndex;i++){           
+            for(int j=i;j>0;j--){    //from right to left              
+                row[j]+=row[j-1];
+            }          
+        }
+        return Arrays.asList(row);
+    }
+	
 }
