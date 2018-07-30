bcdb6610bd7e7be1bba80004ae87be02cb47e437
Alasse <alasse_1988@126.com>
2018-07-30 15:33:05 +0800

    0730

---
+++ Binary Search/Search_Insert_Position_35.java
@@ -0,0 +1,18 @@
+package Binary_Search;
+
+public class Search_Insert_Position_35 {
+	public int searchInsert(int[] nums, int target) {
+        int start=0,end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return start;
+    }
+}
