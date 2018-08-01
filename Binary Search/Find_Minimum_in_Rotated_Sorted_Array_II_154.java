e32c9e8230b2544ef1fef581f994c5ff025cb6ec
Alasse <alasse_1988@126.com>
2018-08-01 15:39:29 +0800

    0801

--- Binary Search/Find_Minimum_in_Rotated_Sorted_Array_II_154.java
+++ Binary Search/Find_Minimum_in_Rotated_Sorted_Array_II_154.java
@@ -1,24 +1,27 @@
 package Binary_Search;
 
-public class Find_Minimum_in_Rotated_Sorted_Array_153 {
-	//Template 2
+public class Find_Minimum_in_Rotated_Sorted_Array_II_154 {
+	//Template 2, Return minimum that may not with minimum index, 0ms
 	public int findMin(int[] nums) {
         if(nums==null||nums.length==0) return 0;
         int start=0,end=nums.length-1;
         while(start<end){
             if(nums[start]<nums[end]) return nums[start];
             int mid=start+(end-start)/2;
-            if(nums[mid]>=nums[start]){  //when only two left, mid==start
+            if((nums[start]==nums[mid])&&(nums[mid]==nums[end])){
+                start++;
+                end--;
+            }else if(nums[start]<=nums[mid]){
                 start=mid+1;
             }else{
                 end=mid;
-                start++; //mid must < start
+                start++;
             }
         }
-        return nums[start];
+        return nums[start];       
     }
 	
-	//Template 2, More concise, only compare mid with end
+	//Template 2, More concise, Return minimum with minimum index, 0ms
 	public int findMin(int[] nums) {
         if(nums==null||nums.length==0) return 0;
         int start=0,end=nums.length-1;
@@ -27,9 +30,14 @@ public class Find_Minimum_in_Rotated_Sorted_Array_153 {
             if(nums[mid]>nums[end]){
                 start=mid+1;
             }else if(nums[mid]<nums[end]){
-                end=mid;
+                end=mid;           
+            }else{     //nums[mid]==nums[end]
+                if(nums[end-1]>nums[end]) //eg:1,2,1,1,1,1
+                    return nums[end]; //return duplicate minimum with minimum index
+                end--;
             }
         }
         return nums[start];
+        
     }
 }
