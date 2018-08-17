1b42f285bda7cf9e408c7382abad616b1e65ea5c
Alasse <alasse_1988@126.com>
2018-08-17 16:26:44 +0800

    0817

---
+++ Binary Search/Pow_x_n_50.java
@@ -0,0 +1,78 @@
+package Binary_Search;
+
+public class Pow_x_n_50 {
+	//Recursion, 14ms
+	public double myPow(double x, int n) {
+        if(n == 0){
+            return 1;
+        }
+        if(n == Integer.MIN_VALUE){
+            n = -(n + 1); // -2^31 -> -(-2^31 + 1) = 2^31 - 1
+            x = 1/x;
+            return x * x * myPow(x * x, n/2);
+
+       }
+        
+        if(n < 0){           
+            n = -n;
+            x = 1 / x;         
+        }
+        
+        return (n % 2 == 0) ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
+    }
+	
+	//Or:
+		public double myPow(double x, int n) {
+        if(n == 0){
+            return 1;
+        }
+        if(n == Integer.MIN_VALUE){
+            n /= 2;
+            x *= x;
+       }       
+        if(n < 0){           
+            n = -n;
+            x = 1 / x;         
+        }
+        
+        return (n % 2 == 0) ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
+    }
+		
+		//Or:
+		public double myPow(double x, int n) {
+	        if(n == 0){
+	            return 1;
+	        }
+	     
+	        if(n < 0){                     
+	            x = 1 / x;         
+	            return (n % 2 == 0) ? myPow(x * x, -(n / 2)) : x * myPow(x * x, -(n / 2));
+	        }
+	        
+	        return (n % 2 == 0) ? myPow(x * x, n / 2) : x * myPow(x * x, n / 2);
+	    }
+	
+	//Iteration, 12ms
+	public double myPow(double x, int n) {
+        if(n == 0){
+            return 1;
+        }
+        double res = 1.0;
+        if(n < 0){         
+            n = -(n + 1); //avoid overflow when n == MIN_VALUE
+            x = 1/x;
+            res *= x;
+        }
+        
+        
+        while(n > 0){
+            if((n & 1) == 1){ //n is odd
+                res *= x;
+            }
+            x *= x;  //x = x^2
+            n >>= 1; //n = n / 2
+            
+        }
+        return res;      
+    }
+}
